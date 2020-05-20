8a07769d776c8a2b21ba57e3baae63d9d6828bb0
StephaneBusiere <stefbubu64@gmail.com>
2020-05-20 19:22:38 +0200

    dragable photo ans svg arrow

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,14 @@ import Slide from 'react-reveal/Slide';
 
 import StarfieldAnimation from 'react-starfield-animation'
 import styles from "./home.module.css"
-import logoSB from '../images/logoStef.svg'
-import stef from '../images/stefmatrixphoto.jpg'
+import sprite from '../images/sprite.svg'
+import stef from '../images/stefPicture.png'
+import hacker from '../images/hacker.jpg'
+import work from '../images/eye.jpg'
 import stefVideo from '../images/matrixStefvideo.mp4'
+import Draggable from 'react-draggable';
+
+
 
 
 class Example extends Component {
@@ -15,27 +20,78 @@ class Example extends Component {
       <div className={styles.outerwrapper}>
      <div className={styles.container}>
       <div className={styles.slide1}>
-      <img className={styles.stefPhoto} src={stef} />
-      <div className={styles.title} >FRONT END DEVELOPPER</div>
-      <object className={styles.logoSB}  data={logoSB}>svg-animation</object>
-      <Link to="#aboutmesection"  className={styles.knowmore}>know more about me</Link>
-        
-      </div>
-      <div id="aboutmesection" className={styles.slide2}>
-      <div>
-    <Slide bottom>
-      <div className={styles.videoContainer}>
       
-      <video  className={styles.stefVideo} src={stefVideo} autoPlay muted loop/>
+      <img className={styles.hacker} src={hacker} />
+      <Link to="#aboutmesection"  className={styles.knowmore}>about me</Link>
+      <div className={styles.headerWrapper}>
+      <div className={styles.header}>
+      <ul className={styles.headerMenu}>
+      <li className={styles.headerMenuitem}>About</li>
+      <li className={styles.headerMenuitem}>Work</li>
+      <li className={styles.headerMenuitem}>Study</li>
+      <li className={styles.headerMenuitem}>Contact</li>
+      <li className={styles.headerMenuItemTitle}>Busiere Stéphane</li>
+      </ul>
       </div>
-      </Slide>
       </div>
-      <Slide right>
-      <div className={styles.textPresentation}></div>
-      </Slide>
+      <div className={styles.intro}>
+      <p className={styles.introText}>
+      As a junior front-end developer, i want to get hire by a entreprise where i can grow my skills and be confortable in a long term adventure.
+      I know that for a recruter it's not easy to hire a junior developer, you don't know what i am capable of and who i really am.
+      it's 
+  </p>
+  <div className={styles.introLinesContainer1}>
+  <div className={styles.introLines}>
+  <span>A</span>
+  
+  </div>
+  </div>
+  <div className={styles.introLinesContainer2}>
+  <div className={styles.introLines}>
+  <span>QUESTION</span>
+  
+  </div>
+  </div>
+  <div className={styles.introLinesContainer3}>
+  <div className={styles.introLines}>
+  <span>OF</span>
+  
+  </div>
+  </div>
+  <div className={styles.introLinesContainer4}>
+  <div className={styles.introLines}>
+  <span>TRUST</span>
+  
+  </div>
+  </div>
       </div>
+      
       </div>
+      
+      <div id="aboutmesection" className={styles.slide2}>
+      <div className={styles.stefPhotosContainer}>
+    <Slide bottom>
+    <img className={styles.stefPhoto2} src={stef} />
+    </Slide>
     
+      
+      <Slide right>
+      <Draggable>
+      
+      <img className={styles.hidePhoto} src={work} />
+      
+      </Draggable>
+      </Slide>
+      </div>
+      
+      </div>
+      <div className={styles.slide3}>
+      <object className={styles.sprite}  data={sprite}>svg-animation</object>
+      <p className={styles.why}>WHY YOU CAN TRUST ME ?</p>
+      </div>
+     
+      </div>
+     
       
       </div>
     )
